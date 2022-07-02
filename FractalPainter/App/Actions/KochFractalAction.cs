e340903543542faffb429f1fc88f4fcf6a380f44
Tr1xxter <cmango67@gmail.com>
2022-07-02 12:49:24 +0500

    Tasks 1-3 done

--- FractalPainter/App/Actions/KochFractalAction.cs
+++ FractalPainter/App/Actions/KochFractalAction.cs
@@ -6,19 +6,16 @@ using Ninject;
 
 namespace FractalPainting.App.Actions
 {
-    public class KochFractalAction : IUiAction, INeed<IImageHolder>, INeed<Palette>
+    public class KochFractalAction : IUiAction
     {
-        private IImageHolder imageHolder;
-        private Palette palette;
+        private readonly IImageHolder imageHolder;
+        private readonly Palette palette;
+        // private readonly KochPainter painter;
 
-        public void SetDependency(IImageHolder dependency)
+        public KochFractalAction(IImageHolder imageHolder, Palette palette)
         {
-            imageHolder = dependency;
-        }
-
-        public void SetDependency(Palette dependency)
-        {
-            palette = dependency;
+            this.imageHolder = imageHolder;
+            this.palette = palette;
         }
 
         public string Category => "Фракталы";
